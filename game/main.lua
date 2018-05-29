c37b5e35ef56ffb8a2514c49081c6c51b65486ca
れみろう <remyroez+github@gmail.com>
2018-05-30 00:11:38 +0900

    :sparkles: アセットの差し替え

--- game/main.lua
+++ game/main.lua
@@ -5,14 +5,14 @@ require 'sbss'
 local sprite
 
 function love.load()
-    sprite = sbss:new('assets/spritesheet.xml')
+    sprite = sbss:new('assets/sprites.xml')
 end
 
 function love.update(dt)
 end
 
 function love.draw()
-    sprite:draw('bunny1_ready.png', 0, 0)
+    sprite:draw('alienGreen_walk1.png', 0, 0)
 end
 
 function love.quit()
