af16c29c70aacb8955176a940fee61a2506e843a
れみろう <remyroez+github@gmail.com>
2018-05-30 01:56:40 +0900

    :sparkles: 地面追加

--- game/main.lua
+++ game/main.lua
@@ -3,9 +3,11 @@ require 'autobatch'
 require 'sbss'
 
 local Player = require 'Player'
+local Ground = require 'Ground'
 
 local sprite
 local player
+local ground
 
 function love.load()
     sprite = sbss:new('assets/sprites.xml')
@@ -13,14 +15,18 @@ function love.load()
     local width, height = love.graphics.getDimensions()
     player = Player(sprite, width * 0.2, height * 0.8)
     player:gotoState 'run'
+
+    ground = Ground(sprite, 500, player.y, width, height)
 end
 
 function love.update(dt)
     player:update(dt)
+    ground:update(dt)
 end
 
 function love.draw()
     player:draw()
+    ground:draw()
 end
 
 function love.keypressed(key, scancode, isrepeat)
