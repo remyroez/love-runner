80c1f66b87438d26c36ae6ecefeaefc53fd9f50e
れみろう <remyroez+github@gmail.com>
2019-04-19 20:37:51 +0900

    :pencil: 読み込み処理やエンティティの生成をすべてシーン内に移した
    更に、詳しいコメントの追記

--- game/main.lua
+++ game/main.lua
@@ -1,72 +1,52 @@
 
+-- グローバルに影響があるライブラリ
 require 'autobatch'
 require 'sbss'
 
-local Player = require 'Player'
-local Ground = require 'Ground'
-local Background = require 'Background'
-local Obstacle = require 'Obstacle'
+-- シーンクラス
 local Scene = require 'Scene'
 
+-- シーン
 local scene
 
+-- 読み込み
 function love.load()
-    -- スプライトの読み込み
-    local sprite = sbss:new('assets/sprites.xml')
-
-    -- サウンドの読み込み
-    local sounds = {
-        bgm = love.audio.newSource('assets/Retro Comedy.ogg', 'stream'),
-        start = love.audio.newSource('assets/coin2.ogg', 'static'),
-        score = love.audio.newSource('assets/coin5.ogg', 'static'),
-        jump = love.audio.newSource('assets/jump3.ogg', 'static'),
-        gameover = love.audio.newSource('assets/gameover3.ogg', 'static')
-    }
-    sounds.bgm:setLooping(true)
-
-    -- スクリーンサイズ
-    local width, height = love.graphics.getDimensions()
-
-    -- プレイヤー
-    local player = Player(sprite, width * 0.2, height * 0.8, 50, 50, sounds)
-    player:gotoState 'run'
-
-    -- 地面
-    local ground = Ground(sprite, 500, player.y, width, height)
-
-    -- 背景
-    local image = love.graphics.newImage('assets/blue_desert.png')
-    local background = Background(image, 100, 0, 0, width, height, height / image:getHeight())
-
-    -- 障害物
-    local obstacle = Obstacle(sprite, 'cactus.png', 500, width + 100, height * 0.8, 50, 80)
-
-    -- シーン
-    scene = Scene(
-        player, obstacle, ground, background, sounds
-    )
-    scene:gotoState 'title'
+    -- シーンの作成とロード
+    scene = Scene()
+    scene:load()
 end
 
+-- 更新
 function love.update(dt)
+    -- シーンの更新
     scene:update(dt)
 end
 
+-- 描画
 function love.draw()
+    -- 画面のリセット
     love.graphics.reset()
+
+    -- シーンの描画
     scene:draw()
 end
 
+-- キー入力
 function love.keypressed(key, scancode, isrepeat)
     if key == 'escape' then
+        -- 終了
         love.event.quit()
     elseif key == 'f5' then
+        -- リスタート
         love.event.quit('restart')
     else
+        -- シーンに処理を渡す
         scene:keypressed(key, scancode, isrepeat)
     end
 end
 
+-- マウス入力
 function love.mousepressed(...)
+    -- シーンに処理を渡す
     scene:mousepressed(...)
 end
